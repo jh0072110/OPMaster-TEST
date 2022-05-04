89248a13c88c26aec452595e5bbd7b4098127311
BigBB2110 <50496854+jh0072110@users.noreply.github.com>
2022-05-05 01:18:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,8 +17,14 @@ rm -rf feeds/luci/applications/luci-app-vssr
 rm -rf feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/xray-plugin
+rm -rf feeds/packages/net/shadowsocks-rust
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/psw2
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/psw
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/pswcore
